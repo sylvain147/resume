07a32f9223352e3be2a3c090616709eee8aee012
sylvain <sylvain.attoumani@gmail.com>
2020-02-18 19:37:47 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>My page</title>
+          <title>My Online Resume</title>
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         </Head>
         <ThemeProvider theme={theme}>
